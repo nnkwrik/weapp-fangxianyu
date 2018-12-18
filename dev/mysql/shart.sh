ed866b450d280674795bafe2fcf638a76915a3dc
nnkwrik <nnkwrik@gmail.com>
2018-12-18 10:26:55 +0900

    mvn打包

--- dev/mysql/shart.sh
+++ dev/mysql/shart.sh
@@ -6,5 +6,5 @@ docker rm fangxianyu-mysql
 docker run -d --name fangxianyu-mysql \
 -v ${cur_dir}/conf:/etc/mysql/conf.d \
 -v ${cur_dir}/data:/var/lib/mysql \
--v ${cur_dir}/log:/var/log/mysql/mysql-bin.log \
+-v ${cur_dir}/log:/var/log/mysql \
 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234  mysql:5.7
