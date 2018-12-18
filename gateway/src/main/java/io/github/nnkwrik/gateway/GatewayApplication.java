5234249b08cf1cb2e8d290f83108dc04d0e1c73c
nnkwrik <nnkwrik@gmail.com>
2018-12-18 09:43:31 +0900

    spring-cloud-gateway替换zuul

---
+++ gateway/src/main/java/io/github/nnkwrik/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package io.github.nnkwrik.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
+
---
+++ gateway/src/test/java/io/github/nnkwrik/gateway/GatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package io.github.nnkwrik.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
